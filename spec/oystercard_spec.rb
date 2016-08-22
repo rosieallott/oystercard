efe830480544487ea96896824ccf4788114e54f2
Rosie Allott <rosie.allott@gmail.com>
2016-08-22 14:40:54 +0100

    Create first test to test default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,8 @@
+require 'oystercard'
+
 describe Oystercard do
-end 
+
+  it 'tests a freshly initialized card has balance of zero' do
+    expect(subject.instance_variable_get(:@balance)).to eq 0
+  end
+end
