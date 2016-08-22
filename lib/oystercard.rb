cc5e7e626823b7ab3068a65103e15e3c506220f0
Rosie Allott <rosie.allott@gmail.com>
2016-08-22 15:58:44 +0100

    Write and pass tests for in_journey, touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,12 @@
 class Oystercard
 
-
 DEFAULT_BALANCE = 0
 MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
     @max = MAXIMUM_BALANCE
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -17,6 +17,18 @@ MAXIMUM_BALANCE = 90
   def deduct(fare)
     fail "Insufficient balance" if @balance - fare < 0
     @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,17 +9,16 @@ describe Oystercard do
   end
 
   describe 'deducting fare' do
-      it 'should deduct money when customer pays for journey' do
-        oystercard.top_up(30)
-        oystercard.deduct(20)
-        expect(oystercard.instance_variable_get(:@balance)).to eq 10
-      end
+    it 'should deduct money when customer pays for journey' do
+      oystercard.top_up(30)
+      oystercard.deduct(20)
+      expect(oystercard.instance_variable_get(:@balance)).to eq 10
+    end
 
-      it 'should not allow the balance to fall below 0' do
-        msg = 'Insufficient balance'
-        expect{oystercard.deduct(1)}.to raise_error msg
-      end
-
+    it 'should not allow the balance to fall below 0' do
+      msg = 'Insufficient balance'
+      expect{oystercard.deduct(1)}.to raise_error msg
+    end
   end
 
   describe 'topping up' do
@@ -32,6 +31,21 @@ describe Oystercard do
       oystercard.top_up(Oystercard::MAXIMUM_BALANCE)
       msg = "Exceed maximum balance of #{Oystercard::MAXIMUM_BALANCE}"
       expect{oystercard.top_up(1)}.to raise_error msg
+    end
+  end
+
+  describe 'status of card i.e. in_journey,touching in & out' do
+    it 'when initialized, is not in journey' do
+      expect(oystercard.in_journey?).to be false
+    end
+    it 'touching in changes in_journey status to true' do
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to be true
+    end
+    it 'touching out (after having touched in) changes in_journey to false' do
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard.in_journey?).to be false
     end
   end
 
