8e68c305355a80fc8d27badf0cc0d039320dfc0c
Joseph <joseph.jr@gmail.com>
2016-08-22 15:19:52 +0100

    maximum amount for topup and max IV

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,25 @@
 class Oystercard
 
-attr_reader :balance
 
 DEFAULT_BALANCE = 0
+MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
+    @max = MAXIMUM_BALANCE
   end
 
   def top_up(amount)
+    fail "Exceed maximum balance of #{@max}" if (@balance + amount) > @max
     @balance += amount
   end
+
+private
+
+attr_reader :balance
+
+  # def max?
+  #   if @balance + > MAXIMUM_BALANCE
+  # end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,9 +8,15 @@ describe Oystercard do
     expect(oystercard.instance_variable_get(:@balance)).to eq 0
   end
 
-  it 'can top up card by top_up method with argument' do
-    oystercard.top_up(30)
-    expect(oystercard.instance_variable_get(:@balance)).to eq 30
+  describe 'topping up' do
+    it 'can top up card by top_up method with argument' do
+      oystercard.top_up(30)
+      expect(oystercard.instance_variable_get(:@balance)).to eq 30
+    end
+
+    it 'should not allow balance to exceed £90' do
+      expect {oystercard.top_up(100)}.to raise_error "Exceed maximum balance of #{@max}"
+    end
   end
 
 end
